48823683531c769d93dc9eeceec2e65bfd720fa1
Mike Killion <killion.kgs@gmail.com>
2015-07-17 11:03:22 -0500

    Set title in default.js

--- config/defaults.js
+++ config/defaults.js
@@ -11,7 +11,7 @@ define({
     //Example of a template specific property. If your template had several color schemes
     //you could define the default here and setup configuration settings to allow users to choose a different
     //color theme.
-    "title": "",
+    "title": "TEMPLATE TEST",
     "summary": "",
     "defaultPanel": "legend",
     "enableDialogModal": false,
@@ -35,12 +35,12 @@ define({
     "swipeInvertPlacement": true,
   	"bitlyLogin": "esri",
     "bitlyKey": "R_65fd9891cd882e2a96b99d4bda1be00e",
-   
+
     //Enter the url to your organizations bing maps key if you want to use bing basemaps
     "bingmapskey": "",
     //Defaults to arcgis.com. Set this value to your portal or organization host name.
     "sharinghost": location.protocol + "//" + "www.arcgis.com",
-    //When true the template will query arcgis.com for default settings for helper services, units etc. If you 
+    //When true the template will query arcgis.com for default settings for helper services, units etc. If you
     "units": null,
     "helperServices": {
         "geometry": {
